3afc86c296cccbc90eb196f50402411979e357ef
GolawskyM <mario712000@yahoo.de>
2022-02-03 19:09:36 +0100

    feat(OkrUserController): added PutMapping

--- src/main/java/de/thbingen/epro/controller/OKRUserController.java
+++ src/main/java/de/thbingen/epro/controller/OKRUserController.java
@@ -1,5 +1,6 @@
 package de.thbingen.epro.controller;
 
+import de.thbingen.epro.exception.NonMatchingIdsException;
 import de.thbingen.epro.model.dto.OkrUserDto;
 import de.thbingen.epro.service.OkrUserService;
 import org.springframework.http.ResponseEntity;
@@ -10,26 +11,27 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class OkrUserController {
 
-    final OkrUserService OkrUserService;
+    private final OkrUserService okrUserService;
 
-    public OkrUserController(OkrUserService OkrUserService) {
-        this.OkrUserService = OkrUserService;
+    public OkrUserController(OkrUserService okrUserService) {
+        this.okrUserService = okrUserService;
     }
 
     @GetMapping
     public List<OkrUserDto> findAll() {
-        return OkrUserService.findAll();
+        return okrUserService.findAll();
     }
 
     @GetMapping("/{id}")
     public OkrUserDto findById(@PathVariable Long id) {
-        Optional<OkrUserDto> result = OkrUserService.findById(id);
+        Optional<OkrUserDto> result = okrUserService.findById(id);
         if (result.isPresent())
             return result.get();
         throw new EntityNotFoundException("No User with this id exists");
@@ -37,22 +39,34 @@ public class OkrUserController {
 
     @PostMapping
     public ResponseEntity<OkrUserDto> addNew(@RequestBody @Valid OkrUserDto newUser) {
-        OkrUserDto OkrUserDto = OkrUserService.saveOkrUser(newUser);
+        OkrUserDto okrUserDto = okrUserService.saveOkrUser(newUser);
         UriComponents uriComponents = UriComponentsBuilder.newInstance()
                 .scheme("http")
                 .host("localhost")
                 .port(8080)
                 .path("/api/v1/users/{id}")
-                .buildAndExpand(OkrUserDto.getId());
-        return ResponseEntity.created(uriComponents.toUri()).body(OkrUserDto);
+                .buildAndExpand(okrUserDto.getId());
+        return ResponseEntity.created(uriComponents.toUri()).body(okrUserDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OkrUserDto> updateById(@PathVariable Long id, @RequestBody @Valid OkrUserDto okrUserDto) {
+        if (okrUserDto.getId() == null)
+            okrUserDto.setId(id);
+        if (!Objects.equals(okrUserDto.getId(), id))
+            throw new NonMatchingIdsException("Ids in path and jsonObject do not match");
+        if (!okrUserService.existsById(id))
+            return this.addNew(okrUserDto);
+
+        return ResponseEntity.ok(okrUserService.saveOkrUser(okrUserDto));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
-        if (!OkrUserService.existsById(id)) {
+        if (!okrUserService.existsById(id)) {
             throw new EntityNotFoundException("No OkrUser with this id exists");
         }
-        OkrUserService.deleteById(id);
+        okrUserService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
