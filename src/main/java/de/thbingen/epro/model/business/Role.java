95a29267c84f383be4a6be833d544af2def484c4
GolawskyM <mario712000@yahoo.de>
2022-02-06 17:52:19 +0100

    fix: Implemented Many-to-Many Relation between Role and Privilege

--- src/main/java/de/thbingen/epro/model/business/Role.java
+++ src/main/java/de/thbingen/epro/model/business/Role.java
@@ -5,8 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-public class Role
-{
+public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,19 +14,24 @@ public class Role
     @Column(nullable = false)
     private String name;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "privilege_id")
-    private Privilege privilege;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "privileges_in_role",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "privilege_id")
+    )
+    private Set<Privilege> privileges;
 
     @OneToMany(targetEntity = OkrUser.class, cascade = CascadeType.ALL)
-    @JoinColumn(name="role_id")
+    @JoinColumn(name = "role_id")
     private Set<OkrUser> OkrUsers = new HashSet<>();
 
     public Role(String name) {
         this.name = name;
     }
 
-    public Role() {}
+    public Role() {
+    }
 
     public Long getId() {
         return id;
@@ -37,16 +41,24 @@ public class Role
         this.id = id;
     }
 
+    public Set<Privilege> getPrivileges() {
+        return privileges;
+    }
+
+    public void setPrivileges(Set<Privilege> privileges) {
+        this.privileges = privileges;
+    }
+
+    public void setOkrUsers(Set<OkrUser> okrUsers) {
+        OkrUsers = okrUsers;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Privilege getPrivilege() {
-        return privilege;
     }
 
     public Set<OkrUser> getOkrUsers() {
