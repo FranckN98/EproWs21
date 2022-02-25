6644220717573d4485e5f9d242777ed690514cd1
GolawskyM <mario712000@yahoo.de>
2022-02-25 22:46:57 +0100

    feat: added more claims to JWT Token

--- src/main/java/de/thbingen/epro/security/SecurityConfiguration.java
+++ src/main/java/de/thbingen/epro/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package de.thbingen.epro.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
