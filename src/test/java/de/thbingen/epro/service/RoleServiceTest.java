a5124c83bdedfdae05565cc00c0e609515b5922d
GolawskyM <mario712000@yahoo.de>
2022-02-28 00:36:57 +0100

    test: added RoleServiceTest

--- src/test/java/de/thbingen/epro/service/RoleServiceTest.java
+++ src/test/java/de/thbingen/epro/service/RoleServiceTest.java
@@ -1,12 +1,14 @@
 package de.thbingen.epro.service;
 
 import de.thbingen.epro.exception.RestExceptionHandler;
-import de.thbingen.epro.model.assembler.PrivilegeAssembler;
+import de.thbingen.epro.model.assembler.RoleAssembler;
 import de.thbingen.epro.model.dto.PrivilegeDto;
+import de.thbingen.epro.model.dto.RoleDto;
 import de.thbingen.epro.model.entity.Privilege;
 import de.thbingen.epro.model.entity.Role;
-import de.thbingen.epro.model.mapper.PrivilegeMapper;
+import de.thbingen.epro.model.mapper.RoleMapper;
 import de.thbingen.epro.repository.PrivilegeRepository;
+import de.thbingen.epro.repository.RoleRepository;
 import de.thbingen.epro.util.CamelCaseDisplayNameGenerator;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
@@ -31,31 +33,35 @@ import java.util.Optional;
 import static de.thbingen.epro.util.SecurityContextInitializer.ReadOnlyUser;
 import static de.thbingen.epro.util.SecurityContextInitializer.initSecurityContextWithUser;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@WebMvcTest(controllers = {PrivilegeService.class},
+@WebMvcTest(controllers = {RoleService.class},
         useDefaultFilters = false,
         includeFilters = {
                 @ComponentScan.Filter(
                         type = FilterType.ASSIGNABLE_TYPE,
-                        value = {PrivilegeService.class, PrivilegeMapper.class, PrivilegeAssembler.class}
+                        value = {RoleService.class, RoleMapper.class, RoleAssembler.class}
                 )
         }
 )
 @Import({RestExceptionHandler.class})
 @AutoConfigureMockMvc(addFilters = false)
 @DisplayNameGeneration(CamelCaseDisplayNameGenerator.class)
-public class PrivilegeServiceTest {
+public class RoleServiceTest {
 
     @Autowired
-    private PrivilegeService service;
+    private RoleService service;
 
     @MockBean
-    private PrivilegeRepository repository;
+    private RoleRepository repository;
 
     @Autowired
-    private PrivilegeMapper mapper;
+    private RoleMapper mapper;
+
+    @MockBean
+    private PrivilegeRepository privilegeRepository;
 
     @Autowired
     private AnnotationLinkRelationProvider annotationLinkRelationProvider;
@@ -64,48 +70,48 @@ public class PrivilegeServiceTest {
     void findAllWithNoRelationShouldReturnOnlySelfLinks() {
         initSecurityContextWithUser(ReadOnlyUser);
 
-        List<Privilege> privileges = List.of(
-                new Privilege(1L, "P1"),
-                new Privilege(1L, "P2")
+        List<Role> roles = List.of(
+                new Role(1L, "R1"),
+                new Role(1L, "R2")
         );
         Pageable pageable = Pageable.ofSize(10);
 
-        when(repository.findAll(pageable)).thenReturn(new PageImpl<>(privileges, pageable, privileges.size()));
-        Page<PrivilegeDto> privilegeDtoPage = service.findAll(pageable);
+        when(repository.findAll(pageable)).thenReturn(new PageImpl<>(roles, pageable, roles.size()));
+        Page<RoleDto> roleDtoPage = service.findAll(pageable);
 
-        assertFalse(privilegeDtoPage.isEmpty());
-        List<PrivilegeDto> privilegeDtos = privilegeDtoPage.getContent();
-        assertEquals("P1", privilegeDtos.get(0).getName());
-        assertEquals("P2", privilegeDtos.get(1).getName());
-        assertEquals("/privileges/?id=1", privilegeDtos.get(0).getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
-        assertEquals("/privileges/?id=1", privilegeDtos.get(1).getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
+        assertFalse(roleDtoPage.isEmpty());
+        List<RoleDto> roleDtos = roleDtoPage.getContent();
+        assertEquals("R1", roleDtos.get(0).getName());
+        assertEquals("R2", roleDtos.get(1).getName());
+        assertEquals("/roles/1", roleDtos.get(0).getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
+        assertEquals("/roles/1", roleDtos.get(1).getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
     }
 
     @Test
     void findByIdReturnsSelfLink() {
         initSecurityContextWithUser(ReadOnlyUser);
 
-        Privilege privilege = new Privilege(1L, "P1");
+        Role role = new Role(1L, "R1");
 
-        when(repository.findById(1L)).thenReturn(Optional.of(privilege));
-        Optional<PrivilegeDto> returned = service.findById(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(role));
+        Optional<RoleDto> returned = service.findById(1L);
 
         assertTrue(returned.isPresent());
-        assertEquals("P1", returned.get().getName());
-        assertEquals("/privileges/?id=1", returned.get().getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
+        assertEquals("R1", returned.get().getName());
+        assertEquals("/roles/1", returned.get().getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
     }
 
     @Test
     void updateShouldUpdateName() {
         initSecurityContextWithUser(ReadOnlyUser);
 
-        Privilege privilege = new Privilege(1L, "Old Name");
-        PrivilegeDto updater = new PrivilegeDto("Updater");
-        when(repository.getById(1L)).thenReturn(privilege);
-        mapper.updatePrivilegeFromDto(updater, privilege);
-        when(repository.save(any(Privilege.class))).thenReturn(privilege);
+        Role role = new Role(1L, "Old Name");
+        RoleDto updater = new RoleDto("Updater");
+        when(repository.getById(1L)).thenReturn(role);
+        mapper.updateRoleFromDto(updater, role);
+        when(repository.save(any(Role.class))).thenReturn(role);
 
-        PrivilegeDto updated = service.updatePrivilege(1L, updater);
+        RoleDto updated = service.updateRole(1L, updater);
         assertEquals(updater.getName(), updated.getName());
         assertDoesNotThrow(() -> updated.getRequiredLink(IanaLinkRelations.SELF));
     }
@@ -114,14 +120,14 @@ public class PrivilegeServiceTest {
     void insertPrivilegeShouldInsertNewPrivilege() {
         initSecurityContextWithUser(ReadOnlyUser);
 
-        PrivilegeDto toBeInserted = new PrivilegeDto("Test Name");
-        Privilege inserted = new Privilege(1L, "Test Name");
-        when(repository.save(any(Privilege.class))).thenReturn(inserted);
+        RoleDto toBeInserted = new RoleDto("Test Name");
+        Role inserted = new Role(1L, "Test Name");
+        when(repository.save(any(Role.class))).thenReturn(inserted);
 
-        PrivilegeDto returned = service.insertPrivilege(toBeInserted);
+        RoleDto returned = service.insertRole(toBeInserted);
 
         assertEquals(toBeInserted.getName(), returned.getName());
-        assertEquals("/privileges/?id=1", returned.getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
+        assertEquals("/roles/1", returned.getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
     }
 
     // region existsById
@@ -145,27 +151,17 @@ public class PrivilegeServiceTest {
     }
 
     @Test
-    void findAllByRoleIdShouldReturnAllItsPrivileges() {
-        initSecurityContextWithUser(ReadOnlyUser);
-
-        List<Privilege> privileges = List.of(
-                new Privilege(1L, "P1"),
-                new Privilege(1L, "P2")
-        );
+    void addingANewPrivilege() {
         Role role = new Role(1L, "R1");
-        role.setPrivileges(new HashSet<>(privileges));
-        Pageable pageable = Pageable.ofSize(10);
+        role.setPrivileges(new HashSet<>());
+        Privilege privilege = new Privilege(1L, "P1");
+        PrivilegeDto newPrivilegeDto = new PrivilegeDto("P1");
+        when(repository.findById(1L)).thenReturn(Optional.of(role));
+        when(repository.save(any(Role.class))).thenReturn(role);
+        when(privilegeRepository.findById(1L)).thenReturn(Optional.of(privilege));
+        service.addNewPrivilege(1L, newPrivilegeDto);
 
-        when(repository.findAllByRoles_Id(1L, pageable)).thenReturn(new PageImpl<>(privileges));
-        Page<PrivilegeDto> returned = service.findAllByRoleId(1L, pageable);
-
-        assertFalse(returned.isEmpty());
-        List<PrivilegeDto> privilegeDtos = returned.getContent();
-        assertEquals("P1", privilegeDtos.get(0).getName());
-        assertEquals("P2", privilegeDtos.get(1).getName());
-        assertEquals("/privileges/?id=1", privilegeDtos.get(0).getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
-        assertEquals("/privileges/?id=1", privilegeDtos.get(1).getRequiredLink(IanaLinkRelations.SELF).toUri().toString());
-
+        assertTrue(role.getPrivileges().contains(privilege));
     }
 
     // endregion
